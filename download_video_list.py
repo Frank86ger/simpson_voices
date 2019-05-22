a1cf249e040036112df59e1a02943500651a28e0
Frank Ehebrecht <Frank-Ehebrecht@gmx.de>
2019-05-22 23:33:22 +0200

    NEW Download video list and time stamps jsons from extern

---
+++ download_video_list.py
@@ -0,0 +1,44 @@
+
+
+import json
+import urllib.request
+import requests
+import os
+# TODO: check and mk raw_data_cutup_jsons
+
+
+def check_and_mkdir(base_path):
+    if not os.path.isdir(os.path.join(base_path, 'raw_data_cutup_jsons')):
+        os.mkdir(os.path.join(base_path, 'raw_data_cutup_jsons'))
+
+
+def get_video_data(base_path):
+
+    check_and_mkdir(base_path)
+    time_stamps_base_addr = r'https://raw.githubusercontent.com/Frank86ger/simpsons_data/master/video_intervals/'
+    video_addr = r'https://raw.githubusercontent.com/Frank86ger/simpsons_data/master/yt_urls.json'
+    web = urllib.request.urlopen(video_addr)
+    yt_urls = json.loads(web.read())
+    out_path = os.path.join(base_path, 'yt_urls.json')
+    save_json(out_path, yt_urls)
+
+    for date in yt_urls:
+        for url in yt_urls[date]:
+            title = url[(url.find('watch?v=')+8):]
+            addr = time_stamps_base_addr + title + '.json'
+            if requests.get(addr).status_code == 200:
+                web = urllib.request.urlopen(addr)
+                out_path = os.path.join(base_path,
+                                        r'raw_data_cutup_jsons',
+                                        title+'.json')
+                save_json(out_path, json.loads(web.read()))
+
+
+def save_json(out_path, json_dict):
+    with open(out_path, 'w+') as f:
+        json.dump(json_dict, f)
+
+
+if __name__ == '__main__':
+    base_path_ = r'/home/frank/Documents/simpson_voices_vers2/'
+    get_video_data(base_path_)
