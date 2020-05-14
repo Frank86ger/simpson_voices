b12363e2ca80fc992d5a417407a12f924b3eb533
frank <frank-ehebrecht@mail.com>
2020-05-14 19:28:19 +0200

    Small tests

--- dataset_reader/dataset_reader.py
+++ dataset_reader/dataset_reader.py
@@ -28,13 +28,58 @@ class H5MultiDataset(torch.utils.data.Dataset):
         with h5py.File(file_path, 'r') as h5f:
             data = h5f['data'][...].astype(float)
             if self.as_fft:
-                data = np.fft.rfft(data).real
+                # data = np.fft.rfft(data).real
+                data = np.abs(np.fft.rfft(data))
             label = h5f['label'][...].astype(float)
 
         return torch.Tensor(data), torch.Tensor(label)
 
     def __len__(self):
         return self.dataset_len
+
+
+# TODO: this way?
+class H5DataLoader(object):
+    def __init__(self, path, as_fft=False, tt_split=0.9, batch_size=10):
+        self.dset = H5MultiDataset(path, as_fft=as_fft)
+        self.train_length = int(tt_split * len(self.dset))
+        self.batch_size = batch_size
+        self._train_loader = None
+        self._test_loader = None
+        self._create_data_loaders()
+
+    def _create_data_loaders(self):
+        train_set, test_set = torch.utils.data.random_split(_dset,
+                                                           [self.train_length, len(self.dset) - self.train_length])
+        # TODO @property? would be nice
+        self._train_loader = torch.utils.data.DataLoader(train_set,
+                                                         batch_size=self.batch_size,
+                                                         shuffle=True,
+                                                         num_workers=4)
+        self._test_loader = torch.utils.data.DataLoader(test_set, batch_size=len(val_set), shuffle=True)
+
+    @property
+    def train_loader(self):
+        return self._train_loader
+
+    @property
+    def test_loader(self):
+        return self._test_loader
+
+    def __repr__(self):
+        # TODO u lazy fuck
+        pass
+
+    """
+        path_ = r'/media/frank/FranksFiles/simps/simps001/datasets/homer_female_02'
+    _dset = H5MultiDataset(path_, as_fft=True)
+    train_length = int(tt_split * len(_dset))
+    train_set, val_set = torch.utils.data.random_split(_dset, [train_length, len(_dset) - train_length])
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=10, shuffle=True, num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=len(val_set), shuffle=True)
+
+    """
+
 
 
 def get_data_loader(dset, batch_size):
--- pytorch_learn/train_net.py
+++ pytorch_learn/train_net.py
@@ -23,6 +23,42 @@ class Network(nn.Module):
         return decision
 
 
+class Trainer(object):
+    def __init__(self, *, data_loader, model, criterion, optimizer):
+        self.data_loader = data_loader
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+
+        self._losses = []
+
+        # TODO: model inp params auto after loading dset? / init model
+        # TODO: dset need more info like size?
+        # TODO: test dset and model
+        # TODO: as_fft outside? yes!
+        # TODO: as_fft, batch_size, tt_split outside for dset object, not path but object is provided?
+        # TODO: dset loader and stuff must be put in dataset reader as convenience methods
+
+    def train(self, *, epochs):
+        self._losses = []
+        for epoch in range(epochs):
+            print(f'Epoch {epoch + 1} / {epoch_count}')
+
+            for batch_ndx, (data, label) in enumerate(train_loader):
+                label_pred = model(data)
+                loss = criterion(label_pred, label)
+                self._losses.append(loss)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+    def test(self):
+        pass
+
+    def save_info(self):
+        raise NotImplementedError
+
+
 if __name__ == "__main__":
     tt_split = 0.9
     # model = Network(2048, 512, 2)
@@ -31,13 +67,14 @@ if __name__ == "__main__":
 
     epoch_count = 60
     criterion = torch.nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)  # model parameter TODO this wont work
 
     # path_ = r'/home/frank/Documents/simpson_voices_9/datasets/moep3'
     # path_ = r'/home/frank/Documents/simpson_voices_11/datasets/test_set_3'
     # path_ = r'/home/frank/Documents/simpson_voices_11/datasets/test_set_5'
     # path_ = r'/home/frank/Documents/simpson_voices_11/datasets/4096_dset'
-    path_ = r'/home/frank/Documents/simpson_voices_11/datasets/homerlisamarge_dset'
+    # path_ = r'/home/frank/Documents/simpson_voices_11/datasets/homerlisamarge_dset'
+    path_ = r'/media/frank/FranksFiles/simps/simps001/datasets/homer_female_02'
     _dset = H5MultiDataset(path_, as_fft=True)
     train_length = int(tt_split * len(_dset))
     train_set, val_set = torch.utils.data.random_split(_dset, [train_length, len(_dset) - train_length])
